3a109bd394d1e0c03b0c6d9e856b43e5d21c3c0c
Kanwar Singh <kanwarsinghrajawat3@gmail.com>
2024-07-05 19:48:57 +0530

    build-testing

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,14 @@
 import express from "express";
-import routes from "./routes";
+import routes from "./api/routes";
 import bodyParser from "body-parser";
 import cors from "cors";
-const PORT = 2703;
+const PORT = 9999;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 routes(app);
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
